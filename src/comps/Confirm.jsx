4d1b1fd5fc31e96e008fd30a2a540bfdab6292cb
Jordan <email.jordanschulte@gmail.com>
2025-03-12 19:25:59 -0400

    final

--- src/comps/Confirm.jsx
+++ src/comps/Confirm.jsx
@@ -10,7 +10,7 @@ const Confirm = ({ userEmail, dismiss }) => {
   return (
     <div className="confirmWrapper">
         <div className='confirm'>
-            <img src="/images/icon-success.svg" alt="" />
+            <img src="/Newsletter-Frontend-Mentor/images/icon-success.svg" alt="" />
             <h1>Thanks for subscribing!</h1>
             <p>A confirmation email has been sent to <b>{userEmail}</b>. Please open it and click the button inside to confirm your subscription.</p>
             <button id='formBtn' onClick={handleDismiss}>Dismiss message</button>
